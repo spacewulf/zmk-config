89e93b31eea2ec905bb7bde9fe0c7b8777863c02
Kees Wolterstorff <kjwolter@umich.edu>
2026-08-28 11:53:54 -0400

    Updated with a lot of changes for the dongle configuration

--- flake.nix
+++ flake.nix
@@ -20,18 +20,55 @@
       packages = forAllSystems (system: rec {
         default = firmware;
 
+        firmware_dongle = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+          name = "firmware_dongle";
+
+          parts = [
+            "dongle"
+          ];
+
+          centralPart = "dongle";
+
+          src = nixpkgs.lib.sourceFilesBySuffices self [
+            ".board"
+            ".cmake"
+            ".conf"
+            ".defconfig"
+            ".dts"
+            ".dtsi"
+            ".json"
+            ".h"
+            ".keymap"
+            ".overlay"
+            ".shield"
+            ".yml"
+            "_defconfig"
+          ];
+
+          board = "nice_nano//zmk";
+          shield = "cosmotyl_%PART%";
+
+          zephyrDepsHash = "sha256-SPgBUgHDMlu7JDLfGQw/68CMnA/GKBHN+3qHp/QFRXY=";
+
+          extraCmakeFlags = [
+            "DSHIELD=settings_reset"
+          ];
+
+          snippets = [
+            "zmk-usb-logging"
+          ];
+        };
+
         firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
           name = "firmware";
 
           parts = [
             "left"
             "right"
-            "dongle"
-            "dongle_settings_reset"
-            "settings_reset"
+            # "dongle"
           ];
 
-          centralPart = "dongle";
+          # centralPart = "dongle";
 
           src = nixpkgs.lib.sourceFilesBySuffices self [
             ".board"
@@ -54,6 +91,10 @@
 
           zephyrDepsHash = "sha256-SPgBUgHDMlu7JDLfGQw/68CMnA/GKBHN+3qHp/QFRXY=";
 
+          extraCmakeFlags = [
+            "-DCONFIG_ZMK_SPLIT=y"
+            "-DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n"
+          ];
           # enableZmkStudio = true;
           # extraCmakeFlags = [
           #   "DSHIELD=settings_reset"
